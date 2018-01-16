0816270d7a97181dfed6784eb0927cd6a5825dc1
Mike Mohrbacher <mike@blackriverinc.com>
2018-01-15 20:02:33 -0600

    + Publish /applications folder + Tootle with 'x-more' style.

---
+++ webroot/applications/clock-app/main.bundle.js
@@ -0,0 +1,376 @@
+webpackJsonp(["main"],{
+
+/***/ "../../../../../src/$$_gendir lazy recursive":
+/***/ (function(module, exports) {
+
+function webpackEmptyAsyncContext(req) {
+	// Here Promise.resolve().then() is used instead of new Promise() to prevent
+	// uncatched exception popping up in devtools
+	return Promise.resolve().then(function() {
+		throw new Error("Cannot find module '" + req + "'.");
+	});
+}
+webpackEmptyAsyncContext.keys = function() { return []; };
+webpackEmptyAsyncContext.resolve = webpackEmptyAsyncContext;
+module.exports = webpackEmptyAsyncContext;
+webpackEmptyAsyncContext.id = "../../../../../src/$$_gendir lazy recursive";
+
+/***/ }),
+
+/***/ "../../../../../src/app/app.component.css":
+/***/ (function(module, exports, __webpack_require__) {
+
+exports = module.exports = __webpack_require__("../../../../css-loader/lib/css-base.js")(false);
+// imports
+
+
+// module
+exports.push([module.i, "app-clock * {\r\n    box-sizing: content-box;\r\n}", ""]);
+
+// exports
+
+
+/*** EXPORTS FROM exports-loader ***/
+module.exports = module.exports.toString();
+
+/***/ }),
+
+/***/ "../../../../../src/app/app.component.html":
+/***/ (function(module, exports) {
+
+module.exports = "<!--The content below is only a placeholder and can be replaced.-->\n<div style=\"text-align:center\">\n  <h1>\n    {{title}}\n  </h1> \n</div>\n<app-clock>\n</app-clock> \n\n"
+
+/***/ }),
+
+/***/ "../../../../../src/app/app.component.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return AppComponent; });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__("../../../core/@angular/core.es5.js");
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+
+var AppComponent = (function () {
+    function AppComponent() {
+        this.title = 'Hey, kids! What time is it?';
+    }
+    return AppComponent;
+}());
+AppComponent = __decorate([
+    Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["n" /* Component */])({
+        selector: 'app-root',
+        template: __webpack_require__("../../../../../src/app/app.component.html"),
+        styles: [__webpack_require__("../../../../../src/app/app.component.css")]
+    })
+], AppComponent);
+
+//# sourceMappingURL=app.component.js.map
+
+/***/ }),
+
+/***/ "../../../../../src/app/app.module.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return AppModule; });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_platform_browser__ = __webpack_require__("../../../platform-browser/@angular/platform-browser.es5.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__angular_core__ = __webpack_require__("../../../core/@angular/core.es5.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__app_component__ = __webpack_require__("../../../../../src/app/app.component.ts");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__clock_clock_component__ = __webpack_require__("../../../../../src/app/clock/clock.component.ts");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_4__clock_time_service__ = __webpack_require__("../../../../../src/app/clock/time.service.ts");
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+
+
+
+
+
+var AppModule = (function () {
+    function AppModule() {
+    }
+    return AppModule;
+}());
+AppModule = __decorate([
+    Object(__WEBPACK_IMPORTED_MODULE_1__angular_core__["L" /* NgModule */])({
+        declarations: [
+            __WEBPACK_IMPORTED_MODULE_2__app_component__["a" /* AppComponent */],
+            __WEBPACK_IMPORTED_MODULE_3__clock_clock_component__["a" /* ClockComponent */]
+        ],
+        imports: [
+            __WEBPACK_IMPORTED_MODULE_0__angular_platform_browser__["a" /* BrowserModule */]
+        ],
+        providers: [__WEBPACK_IMPORTED_MODULE_4__clock_time_service__["a" /* TimeService */], { provide: "TIME_INTERVAL", useValue: 100 }],
+        bootstrap: [__WEBPACK_IMPORTED_MODULE_2__app_component__["a" /* AppComponent */]]
+    })
+], AppModule);
+
+//# sourceMappingURL=app.module.js.map
+
+/***/ }),
+
+/***/ "../../../../../src/app/clock/clock.component.css":
+/***/ (function(module, exports, __webpack_require__) {
+
+exports = module.exports = __webpack_require__("../../../../css-loader/lib/css-base.js")(false);
+// imports
+
+
+// module
+exports.push([module.i, ".clock {\r\n    position: relative;\r\n    font-family: Arial, Helvetica, sans-serif;\r\n    width: 175px;\r\n    height: 175px;\r\n    margin-left: auto;\r\n    margin-right: auto;\r\n}\r\n\r\n.clock > canvas.hidden {\r\n    display:none;\r\n}\r\n\r\n.clock  .digital {\r\n    background-color: rgba(200, 225, 225, 0.5);\r\n    border: 2px inset gray;\r\n    border-radius: 2px;\r\n    left: 0;\r\n    padding-top: 4px;\r\n    position: absolute;\r\n    text-align:center;\r\n    top: 100%;\r\n    width: 100%;\r\n}\r\n", ""]);
+
+// exports
+
+
+/*** EXPORTS FROM exports-loader ***/
+module.exports = module.exports.toString();
+
+/***/ }),
+
+/***/ "../../../../../src/app/clock/clock.component.html":
+/***/ (function(module, exports) {
+
+module.exports = "<div class=\"clock\">\n    <canvas #clockface></canvas>\n    <span class=\"digital\">{{currentTime | date: \"MM-dd-yyyy hh:mm:ss\"}}</span>\n  </div>"
+
+/***/ }),
+
+/***/ "../../../../../src/app/clock/clock.component.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return ClockComponent; });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__("../../../core/@angular/core.es5.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__time_service__ = __webpack_require__("../../../../../src/app/clock/time.service.ts");
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+
+
+var ClockComponent = (function () {
+    function ClockComponent(timeService) {
+        this.timeService = timeService;
+        this.analogVisible = true;
+        this.height = 100;
+        this.width = 100;
+    }
+    ClockComponent.prototype.ngOnInit = function () {
+        var _this = this;
+        this.subscription = this.timeService.tick$.subscribe(function (item) {
+            _this.currentTime = item;
+            _this.drawClock();
+        });
+    };
+    ClockComponent.prototype.ngAfterViewInit = function () {
+        var container = this.clockFace.nativeElement.parentElement;
+        this.height = this.clockFace.nativeElement.height = container.clientHeight;
+        this.width = this.clockFace.nativeElement.width = container.clientWidth;
+        this.radius = (this.height / 2) * 0.90;
+        this.context = this.clockFace.nativeElement.getContext("2d");
+        this.context.translate(this.width / 2, this.height / 2);
+        this.drawClock();
+    };
+    ClockComponent.prototype.ngOnDestroy = function () {
+        this.subscription.unsubscribe();
+    };
+    ClockComponent.prototype.drawFace = function (context, radius) {
+        context.beginPath();
+        context.arc(0, 0, radius, 0, 2 * Math.PI);
+        var bevelGrd = context.createRadialGradient(0, 0, radius * 0.75, 0, 0, radius);
+        bevelGrd.addColorStop(0, "white");
+        bevelGrd.addColorStop(1, "gray");
+        context.fillStyle = bevelGrd;
+        context.fill();
+        // Draw central 'nub'
+        context.beginPath();
+        context.arc(0, 0, radius * 0.075, 0, 2 * Math.PI);
+        context.fillStyle = "black";
+        context.fill();
+    };
+    ClockComponent.prototype.drawNumbers = function (ctx, radius, time) {
+        var ang;
+        var currentHour = time.getHours();
+        var currentMinute = time.getMinutes();
+        var currentSecond = time.getSeconds();
+        ctx.beginPath();
+        ctx.font = radius * 0.15 + "px arial";
+        ctx.textBaseline = "middle";
+        ctx.textAlign = "center";
+        for (var hour = 1; hour < 13; hour++) {
+            ang = hour * Math.PI / 6;
+            ctx.rotate(ang);
+            ctx.translate(0, -radius * 0.85);
+            if (currentSecond / 5 == hour) {
+                ctx.rotate(-ang);
+                ctx.fillText(((hour * 5) % 60).toString(), 0, 0);
+            }
+            else {
+                ctx.fillText(hour.toString(), 0, 0);
+            }
+            if (currentSecond / 5 == hour) {
+                ctx.rotate(ang);
+            }
+            ctx.translate(0, radius * 0.85);
+            ctx.rotate(-ang);
+        }
+    };
+    ClockComponent.prototype.drawHand = function (ctx, pos, length, width) {
+        ctx.beginPath();
+        ctx.lineWidth = width;
+        ctx.lineCap = "round";
+        ctx.moveTo(0, 0);
+        ctx.rotate(pos);
+        ctx.lineTo(0, -length);
+        ctx.stroke();
+        ctx.rotate(-pos);
+    };
+    ClockComponent.prototype.drawTime = function (ctx, radius) {
+        var hour = this.currentTime.getHours();
+        var minute = this.currentTime.getMinutes();
+        var second = this.currentTime.getSeconds();
+        //hour
+        hour = hour % 12;
+        hour = (hour * Math.PI / 6) + (minute * Math.PI / (6 * 60)) + (second * Math.PI / (360 * 60));
+        this.drawHand(ctx, hour, radius * 0.45, radius * 0.07);
+        //minute
+        minute = (minute * Math.PI / 30) + (second * Math.PI / (30 * 60));
+        this.drawHand(ctx, minute, radius * 0.75, radius * 0.07);
+        // second
+        second = (second * Math.PI / 30);
+        this.drawHand(ctx, second, radius * 0.8, radius * 0.02);
+    };
+    ClockComponent.prototype.drawClock = function () {
+        if (this.context != null) {
+            this.drawFace(this.context, this.radius);
+            this.drawNumbers(this.context, this.radius, this.currentTime);
+            this.drawTime(this.context, this.radius);
+        }
+    };
+    return ClockComponent;
+}());
+__decorate([
+    Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["_12" /* ViewChild */])("clockface"),
+    __metadata("design:type", typeof (_a = typeof __WEBPACK_IMPORTED_MODULE_0__angular_core__["u" /* ElementRef */] !== "undefined" && __WEBPACK_IMPORTED_MODULE_0__angular_core__["u" /* ElementRef */]) === "function" && _a || Object)
+], ClockComponent.prototype, "clockFace", void 0);
+ClockComponent = __decorate([
+    Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["n" /* Component */])({
+        selector: 'app-clock',
+        template: __webpack_require__("../../../../../src/app/clock/clock.component.html"),
+        styles: [__webpack_require__("../../../../../src/app/clock/clock.component.css")]
+    }),
+    __metadata("design:paramtypes", [typeof (_b = typeof __WEBPACK_IMPORTED_MODULE_1__time_service__["a" /* TimeService */] !== "undefined" && __WEBPACK_IMPORTED_MODULE_1__time_service__["a" /* TimeService */]) === "function" && _b || Object])
+], ClockComponent);
+
+var _a, _b;
+//# sourceMappingURL=clock.component.js.map
+
+/***/ }),
+
+/***/ "../../../../../src/app/clock/time.service.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return TimeService; });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__("../../../core/@angular/core.es5.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_rxjs_BehaviorSubject__ = __webpack_require__("../../../../rxjs/BehaviorSubject.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_rxjs_BehaviorSubject___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_1_rxjs_BehaviorSubject__);
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+var __param = (this && this.__param) || function (paramIndex, decorator) {
+    return function (target, key) { decorator(target, key, paramIndex); }
+};
+
+
+/// Implement a simple time service to emit an event 
+/// every second containing the current time.
+var TimeService = (function () {
+    function TimeService(interval) {
+        var _this = this;
+        this.ticker = new __WEBPACK_IMPORTED_MODULE_1_rxjs_BehaviorSubject__["BehaviorSubject"](new Date());
+        this.tick$ = this.ticker.asObservable();
+        console.log("Interval = " + interval);
+        window.setInterval(function () {
+            _this.ticker.next(new Date());
+        }, interval);
+    }
+    return TimeService;
+}());
+TimeService = __decorate([
+    Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["B" /* Injectable */])(),
+    __param(0, Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["A" /* Inject */])("TIME_INTERVAL")),
+    __metadata("design:paramtypes", [Object])
+], TimeService);
+
+//# sourceMappingURL=time.service.js.map
+
+/***/ }),
+
+/***/ "../../../../../src/environments/environment.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return environment; });
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+// The file contents for the current environment will overwrite these during build.
+var environment = {
+    production: false
+};
+//# sourceMappingURL=environment.js.map
+
+/***/ }),
+
+/***/ "../../../../../src/main.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__("../../../core/@angular/core.es5.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__angular_platform_browser_dynamic__ = __webpack_require__("../../../platform-browser-dynamic/@angular/platform-browser-dynamic.es5.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__app_app_module__ = __webpack_require__("../../../../../src/app/app.module.ts");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__environments_environment__ = __webpack_require__("../../../../../src/environments/environment.ts");
+
+
+
+
+if (__WEBPACK_IMPORTED_MODULE_3__environments_environment__["a" /* environment */].production) {
+    Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["_19" /* enableProdMode */])();
+}
+Object(__WEBPACK_IMPORTED_MODULE_1__angular_platform_browser_dynamic__["a" /* platformBrowserDynamic */])().bootstrapModule(__WEBPACK_IMPORTED_MODULE_2__app_app_module__["a" /* AppModule */])
+    .catch(function (err) { return console.log(err); });
+//# sourceMappingURL=main.js.map
+
+/***/ }),
+
+/***/ 0:
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__("../../../../../src/main.ts");
+
+
+/***/ })
+
+},[0]);
+//# sourceMappingURL=main.bundle.js.map
